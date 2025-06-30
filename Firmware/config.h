9de7deb03e254d8079bd8db7d4bb982702af8140
TheSeagullDev <67653706+TheSeagullDev@users.noreply.github.com>
2025-06-29 21:54:25 -0400

    Add most of it

---
+++ Firmware/config.h
@@ -0,0 +1,11 @@
+// Copyright 2025 TheSeagullDev (@TheSeagullDev)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#define ENCODER_A_PINS { GP3 }
+#define ENCODER_B_PINS { GP4 }
+
+#define I2C_DRIVER I2CD1
+#define I2C1_SDA_PIN GP6
+#define I2C1_SCL_PIN GP7
+
+#define DIP_SWITCH_PINS { GP2 }
